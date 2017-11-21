5a05fec1d6b8a4719e2196828e42986133c5215c
Thomas Lanzoni <thomas.lanzoni@gmail.com>
2017-11-21 12:51:19 +0100

    Add name to Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :description, :done, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121105341) do
+ActiveRecord::Schema.define(version: 20171121114949) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.boolean  "done"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "name"
   end
 
 end
